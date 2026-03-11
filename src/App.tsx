e279ca3c7520b2c09177e48c0632a3976b398fb5
GabrielSpelchuk <gabrielspelciuc24@gmail.com>
2026-03-11 21:49:31 +0200

    add react memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
 
-import { GoodsList } from './GoodsList';
+import GoodsList from './GoodsList';
 import { Good } from './types/Good';
 import { getAll, get5First, getRedGoods } from './api/goods';
 
